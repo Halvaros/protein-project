326f9ced75ee30a74ad5e6847115a3133e1b742b
Halvaros <36891290+Halvaros@users.noreply.github.com>
2018-02-28 12:55:25 +0100

    Add files via upload

--- classes.py
+++ classes.py
@@ -14,7 +14,7 @@ class Protein:
         self.d= int(p_length)
         self.N= int(g_size)
         self.node = self.d//2
-        self.posArray = array([[self.N/2,i] for i in range((self.N-self.d)//2, (self.N+self.d)//2)],dtype = int)#.transpose()
+        self.posArray = array([[self.N/2,i] for i in range((self.N-self.d)//2, (self.N+self.d)//2)],dtype = int).T
     
     def pRotate(self,pivot, direction):
         if direction == 'counterwise':
@@ -22,19 +22,34 @@ class Protein:
         elif direction == 'clockwise':
             A = array([[0,1],[-1,0]])
         #deler opp posArray i tre deler, prepivot[0], pivot[1] og postpivot[2]
-        temp = vsplit(self.posArray,array([pivot-1,pivot]))
+        temp = hsplit(self.posArray,array([pivot-1,pivot]))
         if pivot > self.node: #pivotmonomer større enn noden
             temp[2] -= temp[1]
-            print(temp[2])
-            #hvis pivot høyere enn noden, roterer ---*--- <-'høyre-hale'
-            temp[2] = (A@temp[2].transpose()).transpose()
+            temp[2] = A@temp[2]
             temp[2] += temp[1]
         elif pivot < self.node: #pivotmonomer mindre enn noden
             temp[0] -= temp[1]
-            temp[0] = (A@temp[0].transpose()).transpose()
+            temp[0] = A@temp[0]
             temp[0] += temp[1]
-        self.posArray = vstack([temp[0], temp[1], temp[2]])
-        
+        self.posArray = hstack([temp[0], temp[1], temp[2]])
+   
+    def legalTwist(self,newArray):
+        return unique(newArray, axis = 1).shape == newArray.shape
+    
+    def getU(self,newArray):
+        U = 0
+        for i in range(newArray.T.shape[0]-2):
+            #print('Monomer',i+1)
+            count = 0
+            for j in range(i+1,newArray.T.shape[0]):
+                #print(newArray.T[j] - newArray.T[i])
+                if linalg.norm(newArray.T[j] - newArray.T[i],1) == 1:
+                    count += 1
+                    #print(count)
+                if count > 1:
+                    U += random.uniform(-10.4e-21,-3.47e-21)
+        return U
+    
     def getPos(self):
         return self.posArray
     
@@ -51,8 +66,10 @@ class Grid:
     def updateGrid(self):
         self.grid[self.grid > 0] = 0
         pos = self.prot.posArray
-        for i in range(len(pos)):
-            self.grid[pos[i,0],pos[i,1]] = i+1
+        for i in range(len(pos[0])):
+            self.grid[pos[0,i],pos[1,i]] = i+1
+
+    
     """def getProt(self):
         return 
     def setGrid(self):
@@ -65,7 +82,7 @@ def d(a):
     if a == 1:
         return 'counterwise'
     return 'clockwise'
-
+"""
 N = 12
 d = 10
 Prt = Protein(d,N)
@@ -74,7 +91,10 @@ Prt.pRotate(6,'counterwise')
 Prt.pRotate(8,'counterwise')
 Prt.pRotate(2,'clockwise')
 Prt.pRotate(5,'counterwise')
-Prt.pRotate(4,'clockwise')
+#2 bonds
+Prt.pRotate(9,'counterwise')
+#doom
+#Prt.pRotate(4,'clockwise')
 
 Grd = Grid(d,N)
 Grd.setProt(Prt)
@@ -84,35 +104,40 @@ print(Grd.getGrid())
 
 print(Prt.posArray)
 
-#######################################
-#mulig endring: endre posArray fra dx2 til 2xd
-################ tar inn 2xd istedenfor dx2###############
+print(Prt.getU(Prt.posArray))
 
+print(Prt.legalTwist(Prt.posArray))
 """
-def updateGrid2(self):
+
+
+################ tar inn 2xd istedenfor dx2###############
+"""
+def updateGrid(self):
         self.grid[self.grid > 0] = 0
         pos = self.prot.posArray
-        for i in range(len(pos[0])):
-            self.grid[pos[0,i],pos[1,i]] = i+1
-            
-def pRotate2(self,pivot, direction):
+        for i in range(len(pos)):
+            self.grid[pos[i,0],pos[i,1]] = i+1
+
+def legalTwist(self,newArray):
+        #print(unique(newArray.transpose(), axis = 1).shape)
+        #print(newArray.transpose().shape)
+        return unique(newArray.transpose(), axis = 1).shape == newArray.transpose().shape
+
+ def pRotate(self,pivot, direction):
         if direction == 'counterwise':
             A = array([[0, -1], [1, 0]])
         elif direction == 'clockwise':
             A = array([[0,1],[-1,0]])
         #deler opp posArray i tre deler, prepivot[0], pivot[1] og postpivot[2]
-        temp = hsplit(self.posArray,array([pivot-1,pivot]))
+        temp = vsplit(self.posArray,array([pivot-1,pivot]))
         if pivot > self.node: #pivotmonomer større enn noden
             temp[2] -= temp[1]
-            temp[2] = A@temp[2]
+            #hvis pivot høyere enn noden, roterer ---*--- <-'høyre-hale'
+            temp[2] = (A@temp[2].transpose()).transpose()
             temp[2] += temp[1]
         elif pivot < self.node: #pivotmonomer mindre enn noden
             temp[0] -= temp[1]
-            temp[0] = A@temp[0]
+            temp[0] = (A@temp[0].transpose()).transpose()
             temp[0] += temp[1]
-        self.posArray = hstack([temp[0], temp[1], temp[2]])
-       
-def legalTwist2(self,newArray):
-        return unique(newArray, axis = 1).shape == newArray.shape
-
-"""
+        self.posArray = vstack([temp[0], temp[1], temp[2]])
+    """
