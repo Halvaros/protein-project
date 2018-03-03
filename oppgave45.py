c515a03a77688641815c463c9d0f1538c799dc1f
BrentKylling <simeny@stud.ntnu.no>
2018-03-03 14:29:54 +0100

    Oppgave 4 5)

--- oppgave45.py
+++ oppgave45.py
@@ -20,30 +20,29 @@ def legalTwist(protein):  # legal i forstanden gyldig struktur,
         twist = protein.tryRotate()
 
 
-def plotIt(X, Y, ax=None, T=None):
-    if T == None:
+def plotIt(X, Y, ax=None, N=None):
+    if ax == None:
         plt.figure()
         plt.plot(X, Y)
         plt.grid()
     else:
-        ax.set_title(str(T) + ' K')
-        ax.set_ylim(auto=True)  # ([-3.47e-21,-10.4e-21])
+        ax.set_title(str(N))
+        ax.grid()
+        ax.invert_xaxis()
         ax.plot(X, Y)
 
 
-def markerPlot(xMatrix, yMatrix):
+def markerPlot(xMatrix, yMatrix, ax):
     xMarkers = xMatrix[:, -1]
     yMarkers = yMatrix[:, -1]
-    otherXMarkers=xMatrix[:,0]
-    otherYmarkers=yMatrix[:,0]
+    otherXMarkers = xMatrix[:, 0]
+    otherYmarkers = yMatrix[:, 0]
     xMatrix = xMatrix.flatten()
     yMatrix = yMatrix.flatten()
-    plt.figure()
-    plt.plot(xMatrix, yMatrix,linewidth=0.2)
-    #plt.plot(otherXMarkers,otherYmarkers,marker='o',color='y')
-    #plt.plot(xMarkers, yMarkers,marker='o', color='g')
-    plt.plot((xMarkers+otherXMarkers)/2,(yMarkers+otherYmarkers)/2,marker='o',color="r")
-    plt.grid()
+    ax.plot(xMatrix, yMatrix, linewidth=0.2)
+    # ax.plot(otherXMarkers,otherYmarkers,marker='o',color='y')
+    # ax.plot(xMarkers, yMarkers,marker='o', color='g')
+    ax.plot((xMarkers + otherXMarkers) / 2, (yMarkers + otherYmarkers) / 2, marker='o', color="r")
 
 
 def averageIt(arr, dArr):
@@ -52,48 +51,45 @@ def averageIt(arr, dArr):
     return arr
 
 
-#################################################
-gridN = 20
-N = 15
-dMax = 10000
+def chillax(N, gridN):
+    epsilon = zeros((Nt, d))
+    L = zeros((Nt, d))
+    L[1, 1] = N
+    Prt = Protein(N, gridN, TArr[0])
+    for i in range(Nt):
+        for j in range(d):
+            legalTwist(Prt)
+            # showIt(Grd)
+            epsilon[i, j] = Prt.U
+            L[i, j] = Prt.L
+            # print(Prt.U)
+        Prt.T = TArr[i]
 
+    meanEpsilon = averageIt(epsilon.copy(), d)
+    meanL = averageIt(L, d)
+    return epsilon, meanEpsilon, meanL
+
+
+NArr = array([15, 30])
+gridNArr = array([20, 35])
 # s=1E-6 #for Nt=15
 s = 1E-6
-# sArr = array([1E-6])  # linspace(1E-6, 1E-4, 3)
-
-#################################################
-
-# Gradvis avkjøling
-
 d = 600
 TArr = arange(1500, -30, -30)
 Nt = len(TArr)
-epsilon = zeros((Nt, d))
 dArr = arange(1, d * Nt + 1).reshape((Nt, d))
-L = zeros((Nt, d))
-L[1, 1] = N
-Prt = Protein(N, gridN, TArr[0])
 
-for i in range(Nt):
-    for j in range(d):
-        legalTwist(Prt)
-        # showIt(Grd)
-        epsilon[i, j] = Prt.U
-        L[i, j] = Prt.L
-        # print(Prt.U)
-    Prt.T = TArr[i]
+fig1, axArr1 = plt.subplots(1, 2, sharex=True)
+fig2, axArr2 = plt.subplots(1, 2, sharex=True)
+fig3, axArr3 = plt.subplots(1, 2, sharex=True)
+for i in range(2):
+    epsilon, meanEpsilon, meanL = chillax(NArr[i], gridNArr[i])
+    markerPlot(dArr, epsilon, axArr1[i])
+    plotIt(TArr, meanEpsilon, axArr2[i], NArr[i])
+    plotIt(TArr, meanL, axArr3[i], NArr[i])
 
-# print(dArr)
+    # Grd = Grid(d, gridN, Prt)
+    # while True:
+    #    showIt(Grd)
 
-meanEpsilon = averageIt(epsilon.copy(), d)
-meanL = averageIt(L, d)
-markerPlot(dArr, epsilon)
-plotIt(TArr, meanEpsilon)
-plt.gca().invert_xaxis()
-plotIt(TArr, meanL)
-plt.gca().invert_xaxis()
-
-#Grd = Grid(d, gridN, Prt)
-#while True:
-#    showIt(Grd)
 plt.show()
